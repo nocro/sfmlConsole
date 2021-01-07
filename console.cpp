aec1ad197d881614c1bf16a282e298a80bbbf96f
Simon Monteiro <simon.monteiro@u-psud.fr>
2021-01-08 00:40:43 +0100

    windows console

--- console.cpp
+++ console.cpp
@@ -8,12 +8,23 @@ Console::Console(): text_out("",font), text_in("",font), last_string_printed(0)
     font.loadFromFile("arial.ttf");
 
     modes.push_back(sf::VideoMode::getFullscreenModes()[0]);
+    activated_mode = 0;
     if(!modes[0].isValid())
     {
         std::cout << "attention ce mode n'est pas valid\n";
+        modes.push_back(sf::VideoMode(800, 600));
+        activated_mode = 1;
+        if (!modes[1].isValid())
+        {
+            std::cout << "bon mdr wtf" << std::endl;
+        }
+        else
+        {
+            std::cout << "tout va bien" << std::endl;
+        }
     }
-    window.create(modes[0],"loup garou");
-    activated_mode = 0;
+    window.create(modes[activated_mode],"loup garou");
+    
 
     console_params param_mode;
     param_mode.window_size = window.getSize();
@@ -52,16 +63,12 @@ void Console::add(std::string s)
     if(out.size()>27)
     {
         /** update de la taille puis de la position de la scroling bar **/
-        
         float y = modes_param[activated_mode].scroling_bar_size.y;
         float ysize = (27.0/static_cast<float>(out.size()))*y ;
-        scroling_bar.setSize(sf::Vector2f( modes_param[activated_mode].scroling_bar_size.x 
-                            , ysize));
-
+        scroling_bar.setSize(sf::Vector2f( modes_param[activated_mode].scroling_bar_size.x , ysize));
         float distance = (static_cast<float>( out.size() - last_string_printed) / static_cast<float>(out.size()-27));
         float oy = (1.0-distance) * (y-ysize);
-        scroling_bar.setOrigin(sf::Vector2f( modes_param[activated_mode].scroling_bar_origin.x
-                            , -oy));
+        scroling_bar.setOrigin(sf::Vector2f( modes_param[activated_mode].scroling_bar_origin.x, -oy));
     }
 }
     
@@ -70,12 +77,13 @@ void Console::draw()
 {
     window.clear();
     
-    std::string toprint="";
+    std::string toprint=" ";
     auto m = std::max(0,static_cast<int>(last_string_printed-27));
     for(long unsigned int i= m; i<last_string_printed; i++ )
     {
         toprint += out[i] +"\n";
     }
+    std::cout << " caca : " << toprint.size() << std::endl;
     text_out.setString(toprint);
     window.draw(text_out);
     window.draw(tapping_zone);
--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ int main()
     
     while(console.isOpen())
     {
+        std::cout << "etape 1\n";
         console.event();
+        std::cout << "etape 2\n";
         console.draw();
+        std::cout << "etape 3\n";
         console.update();
+        std::cout << "etape 4\n";
     }
     return 0;
 }
